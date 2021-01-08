4001b5f050f642b36fa21fd16dbed532315484fd
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2021-01-08 19:17:26 +0100

    Add FreeBSD installer

---
+++ install_freebsd.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp -r ./usr/share /usr/local/
+cp -r ./usr/lib /usr/local/
+cp -r ./usr/local/bin /usr/local/
+cp ./etc/arcologout.conf /usr/local/etc/arcologout.conf
+echo Done!
+echo "Don't forget to install the python dependencies: sudo pkg install python3 py37-cairo py37-gobject3 libwnck3"
